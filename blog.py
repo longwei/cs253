d1311245d0b947bda4b6f3bac07fbe344ba75f94
Longwei Su <longwei.su@gmail.com>
2013-03-11 15:50:45 -0400

    update unit4

--- blog.py
+++ blog.py
@@ -3,6 +3,8 @@ import re
 from string import letters
 import webapp2
 import jinja2
+import hashlib
+import hmac
 
 from google.appengine.ext import db
 
@@ -18,6 +20,34 @@ def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
     response.out.write(post.content)
 
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw):
+    salt = make_salt()
+    s = "%s,%s" % (hashlib.sha256(name + pw + salt).hexdigest(), salt)
+    return s
+def valid_pw(name, pw, h):
+    ###Your code here
+    salt = h.split(",")[1]
+    return (h.split(",")[0] == hashlib.sha256(name + pw + salt).hexdigest())
+
+# h = make_pw_hash('spez', 'hunter2')
+# print valid_pw('spez', 'hunter2', h)
+
+SECRET = 'mySecret'
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    s = h.split("|")[0]
+    if ( h == make_secure_val(s)):
+        return s
+
+
 # webapp2.request is the generic request handler
 class BlogHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -26,26 +56,75 @@ class BlogHandler(webapp2.RequestHandler):
         self.write(render_str(template, **kw))
     def render_str(template, **params):
         return render_str(template, **params)
-   
+
 #unit 4
+class Login(BlogHandler):
+    def get(self):
+        username_cookie = self.request.cookies.get('username')
+        if username_cookie: 
+            self.redirect('/blog/welcome?username=' + username_cookie)
+        self.render("login.html", username = "longwei", password = "123")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('password')
+        key = db.Key.from_path('account', str(username), parent=account_key())
+
+        if key:
+            self.write(key.id())
+            self.write(key.name())
+            self.write("**")
+        else:
+            self.write("can't find the key") 
+        
+        # self.write(key)  
+        account = db.get(key)
+        if account:
+            self.write(account.toString())
+        else:
+            self.write("can't find the account") 
+        # if account:
+        #     # self.redirect('/blog/welcome?username=' + username)
+        #     self.write("invalid account\n")
+        # else:
+        #     self.write("invalid account\n") 
+        
+
+class Account(db.Model):
+    username = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty
+    @classmethod
+    def get_id(cls, uid):
+        return Account.get_id
+    def login(cls, name, pw):
+
 class MainPage(BlogHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
+        visits = 0
         self.write("Hello CS253\n") 
-        visits = self.request.cookies.get('visits', '0')
-        # session demo
-        if visits.isdigit():
-            visits = int(visits) + 1
+        visits_cookie_str = self.request.cookies.get('visits')
+        if visits_cookie_str:
+            cookies_val = check_secure_val(visits_cookie_str)
+            self.write("***\n") 
+            if cookies_val:
+                visits = int(cookies_val)
+        visits += 1
+        new_cookie_val = make_secure_val(str(visits))
+        self.response.headers.add_header('Set-Cookie', 'visits= %s' % new_cookie_val)
+        if visits == 100005:
+            self.write("You are the best") 
         else:
-            visits = 0
-        self.response.headers.add_header('Set-Cookie', 'visits= %s' % visits)
-        
-        self.write("You have been here for %s times" % visits) 
+            self.write("You have been here for %s times" % visits) 
 
 #blog unit3
 #database
 def blog_key(name = 'default'):
     return db.Key.from_path('blogs',name)
+def account_key(name = 'default'):
+    return db.Key.from_path('account',name)
 
 # entity or table
 class Post(db.Model):
@@ -109,6 +188,9 @@ class Welcome(BlogHandler):
 
 class Signup(BlogHandler):
     def get(self):
+        username_cookie = self.request.cookies.get('username')
+        if username_cookie: 
+            self.redirect('/blog/welcome?username=' + username_cookie)
         self.render("signup-form.html")
 
     def post(self):
@@ -120,7 +202,11 @@ class Signup(BlogHandler):
 
         params = dict(username = username, email = email)
 
-        if not valid_username(username):
+        if  self.usrname_used(username):
+            params['error_username'] = "Username used."
+            have_error = True
+
+        if not valid_username(username) :
             params['error_username'] = "That's not a valid username."
             have_error = True
 
@@ -139,7 +225,16 @@ class Signup(BlogHandler):
             print "error"
             self.render('signup-form.html', **params)
         else:
-            self.redirect('/unit2/welcome?username=' + username)
+            u = Account(parent = account_key(), username = username, 
+                    password = password, email = email)
+            # TODO
+            # make_pw_hash(username, password)
+            u.put()
+            self.response.headers.add_header('Set-Cookie', 'username= %s, Path=/' % str(username) )
+            self.redirect('/blog/welcome?username=' + username)
+    def usrname_used(self, un):
+        u = db.GqlQuery("select * from Account where username = :1", un)
+        return u.get()
 
 class SearchPage(BlogHandler):
     def get(self):
@@ -161,7 +256,7 @@ class TestHandle(webapp2.RequestHandler):
         # self.response.out.write(q)
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write(self.request)
-        self.redirect('/')
+        self.redirect('/static/test.xls')
 
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
@@ -178,12 +273,13 @@ def valid_email(email):
         
 
 # url mapping 
-app = webapp2.WSGIApplication([('/', SearchPage),
+app = webapp2.WSGIApplication([('/', MainPage),
                                ('/testform', TestHandle),
                                ('/unit4', MainPage),
                                ('/unit2/rot13', Rot13),
-                               ('/unit2/signup', Signup),
-                               ('/unit2/welcome', Welcome),
+                               ('/blog/signup', Signup),
+                               ('/blog/login', Login),
+                               ('/blog/welcome', Welcome),
                                ('/blog/?', BlogFront),
                                ('/blog/([0-9]+)', PostPage),
                                ('/blog/newpost', NewPost)
